98b9a505afe51969f16f020f77aa78ce0b62efce
CarlosEduardo2033 <51630607+CarlosEduardo2033@users.noreply.github.com>
2020-03-10 09:02:05 -0300

    a
    a

---
+++ Carro.java
@@ -0,0 +1,57 @@
+
+    public class Carro {
+        public String cor;
+        public int ano;
+        public String marca;
+        public float potencia;
+        public float quilometragem;
+        public String placa;
+
+        public String getCor() {
+            return cor;
+        }
+
+        public void setCor(String cor) {
+            this.cor = cor;
+        }
+
+        public int getAno() {
+            return ano;
+        }
+
+        public void setAno(int ano) {
+            this.ano = ano;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public float getPotencia() {
+            return potencia;
+        }
+
+        public void setPotencia(float potencia) {
+            this.potencia = potencia;
+        }
+
+        public float getQuilometragem() {
+            return quilometragem;
+        }
+
+        public void setQuilometragem(float quilometragem) {
+            this.quilometragem = quilometragem;
+        }
+
+        public String getPlaca() {
+            return placa;
+        }
+
+        public void setPlaca(String placa) {
+            this.placa = placa;
+        }
+    }
